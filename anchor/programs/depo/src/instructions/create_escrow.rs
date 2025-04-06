18eefd6a19aff6d1762cc8325434e759b2939492
Maxime Normandin <web2@londerosports.com>
2025-04-06 19:28:02 -0400

    start create_escrow

--- anchor/programs/depo/src/instructions/create_escrow.rs
+++ anchor/programs/depo/src/instructions/create_escrow.rs
@@ -1,1 +1,25 @@
 use anchor_lang::prelude::*;
+use crate::states::{Escrow, Status};
+
+pub fn create_escrow(
+    ctx: Context<CreateEscrow>,
+    name: String,
+    description: String,
+    
+) -> Result<()> {
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct CreateEscrow<'info> {
+    #[account(
+      init,
+      payer = signer.
+      space = // TODO: implement InitSpace
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
