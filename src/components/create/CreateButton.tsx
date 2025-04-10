e0776a04e04c9cdbd3caeb4d19f41696dce0b46b
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-10 15:52:41 +0200

    src/components/create/CreateButton.tsx = first version of CreateButton.tsx passing on data to -create page-

---
+++ src/components/create/CreateButton.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function CreateButton() {
+  const router = useRouter()
+  return (
+    <button
+      onClick={() => router.push('/create')}
+      className="px-6 py-3 border-2 border-black text-black bg-white rounded-lg hover:bg-gray-100 transition"
+    >
+      Create Escrow
+    </button>
+  )
+}
