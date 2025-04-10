b9b6ee242120c13e42bcdc81dfbae592541554c1
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-10 16:29:29 +0200

    DepoCard.tsx | CreateButton.tsx = update components to isolate CreateButton as independant component

--- src/components/create/CreateButton.tsx
+++ src/components/create/CreateButton.tsx
@@ -9,7 +9,7 @@ export default function CreateButton() {
       onClick={() => router.push('/create')}
       className="px-6 py-3 border-2 border-black text-black bg-white rounded-lg hover:bg-gray-100 transition"
     >
-      Create Escrow
+      Create DEPO
     </button>
   )
 }
