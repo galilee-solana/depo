4974e93cdc4d86397e0c00f21f3c3e3e08041f97
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-10 15:52:41 +0200

    src/components/create/CreateButton.tsx = first version of CreateButton.tsx passing on data to -create page-

--- src/components/create/CreateButton.tsx
+++ src/components/create/CreateButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+<<<<<<< HEAD
 import React from 'react'
 import { useDepoClient } from '@/contexts/useDepoClientCtx'
 import toast from 'react-hot-toast'
@@ -30,6 +31,17 @@ export default function CreateButton() {
       className="px-6 py-3 border-2 border-black text-black bg-white rounded-lg hover:bg-gray-100 transition"
     >
       Create DEPO
+=======
+
+export default function CreateButton() {
+  const router = useRouter()
+  return (
+    <button
+      onClick={() => router.push('/create')}
+      className="px-6 py-3 border-2 border-black text-black bg-white rounded-lg hover:bg-gray-100 transition"
+    >
+      Create Escrow
+>>>>>>> e0776a0 (src/components/create/CreateButton.tsx = first version of CreateButton.tsx passing on data to -create page-)
     </button>
   )
 }
