1ddb8b971034a9ae3758efbeedb4ffa36415424d
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-10 11:39:33 +0200

    ui-layout.tsx = remove old function appHero | DepoCard.tsx = update variable name to Escrow...

--- src/components/ui/ui-layout.tsx
+++ src/components/ui/ui-layout.tsx
@@ -102,28 +102,6 @@ export function AppModal({
   )
 }
 
-//export function AppHero({
-  //children,
-  //title,
-  //subtitle,
-//}: {
-  //children?: ReactNode
-  //title: ReactNode
-  //subtitle: ReactNode
-//}) {
-  //return (
-    //<div className="w-screen h-screen flex flex-col hero py-[64px]">
-      //<div className="hero-content text-center">
-        //<div className="max-w-2xl">
-          //{typeof title === 'string' ? <h1 className="text-5xl font-bold">{title}</h1> : title}
-          //{typeof subtitle === 'string' ? <p className="py-6">{subtitle}</p> : subtitle}
-          //{children}
-        //</div>
-      //</div>
-    //</div>
-  //)
-//}
-
 export function ellipsify(str = '', len = 4) {
   if (str.length > 30) {
     return str.substring(0, len) + '..' + str.substring(str.length - len, str.length)
