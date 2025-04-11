c61d69b620df1d359f7066ba3d210f042b342426
Maxime Normandin <web2@londerosports.com>
2025-04-11 17:25:21 -0400

    add 1st remove_recipient test

--- anchor/tests/remove_recipient_test.spec.ts
+++ anchor/tests/remove_recipient_test.spec.ts
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { strict as assert } from 'assert'
 
 
-describe('DEPO - Instruction: remove_recipient', () => {
+describe('Test - Instruction: remove_recipient', () => {
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider)
   
@@ -35,10 +35,83 @@ describe('DEPO - Instruction: remove_recipient', () => {
     assert(balance2 > 0, 'Airdrop failed')
   })
 
-  it('Successfully add and remove recipient and update escrow state', async () => {});
-  it('Prevent removal of non-existent recipient', async () => {});
-  it('Prevent removal on escrow with 0 recipients', async () => {});
-  it('Enforce initializer-only access control', async () => {});
+  it('Successfully Remove recipient and update escrow state', async () => {
+    const uuid = uuidv4().replace(/-/g, '')
+    const escrowId = Uint8Array.from(Buffer.from(uuid, 'hex'))
+
+    const [escrowKey, _escrowBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from('escrow'), escrowId],
+      program.programId
+    )
+
+    const testName = 'TEST_ESCROW name'
+    const name = Buffer.from(testName, 'utf8')
+
+    const testDescription = 'TEST_ESCROW description'
+    const description = Buffer.from(testDescription, 'utf8')
+
+    await program.methods.createEscrow(
+      Array.from(escrowId),
+      name,
+      description
+    )
+    .accounts({
+      escrow: escrowKey,
+      signer: initializer.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([initializer])
+    .rpc()
+    
+    let escrowAccount = await program.account.escrow.fetch(escrowKey)
+    expect(escrowAccount.recipientsCount).toBe(0)
+
+    const [recipientKey, recipientBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from('recipient'), escrowKey.toBuffer(), recipientWallet.publicKey.toBuffer()],
+      program.programId
+    )
+
+    await program.methods.addRecipient(
+      Array.from(escrowId),
+      recipientWallet.publicKey
+    )
+    .accounts({
+      escrow: escrowKey,
+      recipient: recipientKey,
+      initializer: initializer.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([initializer])
+    .rpc()
+
+    escrowAccount = await program.account.escrow.fetch(escrowKey)
+    expect(escrowAccount.recipientsCount).toBe(1)
+
+    await program.methods.removeRecipient(
+      Array.from(escrowId),
+      recipientWallet.publicKey
+    )
+    .accounts({
+      escrow: escrowKey,
+      recipient: recipientKey,
+      initializer: initializer.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([initializer])
+    .rpc()
+
+    escrowAccount = await program.account.escrow.fetch(escrowKey)
+    expect(escrowAccount.recipientsCount).toBe(0)
+
+    try {
+      await program.account.recipient.fetch(recipientKey)
+      throw new Error('Recipient account should not exist')
+    } catch (error: any) {
+      expect(error.message).toContain(`Account does not exist or has no data ${recipientKey.toBase58()}`)
+    }
+  });
+  it('fails when removing a non-existent recipient', async () => {});
+  it('fails when removing a recipient from an escrow with 0 recipients', async () => {});
   it('Enforce Draft status requirement', async () => {
     // Can't be tested since we don't have a way to change the status of the escrow
     // TODO: Add a test for this once we have the started instruction
