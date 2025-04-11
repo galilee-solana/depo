4d2f43e8fa5823833beeb95380aad3433f49843b
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-11 14:01:27 +0200

    CreateButton.tsx = modify component to go to create/page.tsx

--- src/components/create/CreateButton.tsx
+++ src/components/create/CreateButton.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import { useState } from 'react'
-import { CreateEscrowForm } from './CreateEscrowForm'
+import { useRouter } from 'next/navigation'
+import React from 'react'
 
-export default function CreateButton({ onStartCreating }: { onStartCreating: () => void }) {
+export default function CreateButton() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/create') // Route to page = src/app/create/page.tsx
+  }
+
   return (
     <button
-      onClick={onStartCreating}
+      onClick={handleClick}
       className="px-6 py-3 border-2 border-black text-black bg-white rounded-lg hover:bg-gray-100 transition"
     >
       Create DEPO
     </button>
   )
 }
-
