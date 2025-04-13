a931613920ad42369c89b9b6b91c3adaf30ff6d1
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-13 13:25:27 +0200

    create routes for escrow/create/add_description | add_single_approval | add_multisig_approval | set new components for inputs : ToogleInputFieldDateTime.tsx , ToogleInputFieldNumber.tsx, ToogleInputFieldSolAddress.tsx

--- src/app/escrow/create/page.tsx
+++ src/app/escrow/create/page.tsx
@@ -3,6 +3,8 @@
 import { useWallet } from '@solana/wallet-adapter-react'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import ToogleInputFieldNumber from '@/components/create/ToogleInputFieldNumber'
+import ToogleInputFieldDateTime from '@/components/create/ToogleInputFieldDateTime'
 
 export default function CreateEscrow() {
   const { publicKey } = useWallet()
@@ -55,86 +57,53 @@ export default function CreateEscrow() {
 
   return (
     <div className="p-6 space-y-4">
-      <h1 className="text-2xl font-bold">Create new Escrow</h1>
-      <p>Your pubKey is : {publicKey.toBase58()}</p>
+      <h1 className="text-2xl font-bold">Create your new DEPO.</h1>
 
       <input
         placeholder="Deposit name #1"
-        className="w-full border px-3 py-2 rounded"
+        className="w-full px-3 py-2 border-2 border-black rounded-2xl bg-white text-black"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
 
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          checked={startTimeEnabled}
-          onChange={() => setStartTimeEnabled(!startTimeEnabled)}
-        />
-        <span>Enable Start Time</span>
-      </label>
-      {startTimeEnabled && (
-        <input
-          placeholder="Start Time"
-          className="w-full border px-3 py-2 rounded"
-          value={startTime}
-          onChange={(e) => setStartTime(e.target.value)}
-        />
-      )}
+      <ToogleInputFieldDateTime
+        label="Start Time"
+        placeholder="Deposit start time"
+        enabled={startTimeEnabled}
+        setEnabled={setStartTimeEnabled}
+        value={startTime}
+        setValue={setStartTime}
+      />
 
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          checked={timelockEnabled}
-          onChange={() => setTimelockEnabled(!timelockEnabled)}
-        />
-        <span>Enable Timelock</span>
-      </label>
-      {timelockEnabled && (
-        <input
-          placeholder="Timelock"
-          className="w-full border px-3 py-2 rounded"
-          value={timelock}
-          onChange={(e) => setTimelock(e.target.value)}
-        />
-      )}
+      <ToogleInputFieldDateTime
+        label="Time Lock"
+        placeholder="Time Lock"
+        enabled={timelockEnabled}
+        setEnabled={setTimelockEnabled}
+        value={timelock}
+        setValue={setTimelock}
+      />
 
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          checked={minimumAmountEnabled}
-          onChange={() => setMinimumAmountEnabled(!minimumAmountEnabled)}
-        />
-        <span>Enable Minimum Amount</span>
-      </label>
-      {minimumAmountEnabled && (
-        <input
-          placeholder="Minimum Amount"
-          className="w-full border px-3 py-2 rounded"
-          value={minimumAmount}
-          onChange={(e) => setMinimumAmount(e.target.value)}
-        />
-      )}
+      <ToogleInputFieldNumber
+        label="Minimum Amount"
+        placeholder="Minimum Amount"
+        enabled={minimumAmountEnabled}
+        setEnabled={setMinimumAmountEnabled}
+        value={minimumAmount}
+        setValue={setMinimumAmount}
+      />
 
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          checked={targetAmountEnabled}
-          onChange={() => setTargetAmountEnabled(!targetAmountEnabled)}
-        />
-        <span>Enable Target Amount</span>
-      </label>
-      {targetAmountEnabled && (
-        <input
-          placeholder="Target Amount"
-          className="w-full border px-3 py-2 rounded"
-          value={targetAmount}
-          onChange={(e) => setTargetAmount(e.target.value)}
-        />
-      )}
+      <ToogleInputFieldNumber
+        label="Target Amount"
+        placeholder="Target Amount"
+        enabled={targetAmountEnabled}
+        setEnabled={setTargetAmountEnabled}
+        value={targetAmount}
+        setValue={setTargetAmount}
+      />
 
       <button
-        onClick={() => setDescription('Add a description message')}
+        onClick={() => router.push('/escrow/create/add_description')}
         className="text-left text-black flex items-center space-x-2"
       >
         <span>➕</span>
@@ -142,7 +111,7 @@ export default function CreateEscrow() {
       </button>
 
       <button
-        onClick={() => setSingleApproval(!singleApproval)}
+        onClick={() => router.push('/escrow/create/add_single_approval')}
         className="text-left text-black flex items-center space-x-2"
       >
         <span>➕</span>
@@ -150,7 +119,7 @@ export default function CreateEscrow() {
       </button>
 
       <button
-        onClick={() => setMultisigApproval('Add multisig address')}
+        onClick={() => router.push('/escrow/create/add_multisig_approval')}
         className="text-left text-black flex items-center space-x-2"
       >
         <span>➕</span>
