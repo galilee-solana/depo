d37932ff804a47ddd69fd772d9bbdfc73bfb839b
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-14 16:18:57 +0200

    keep track of set_depositor_address | set_recipient_address | add_single_approval | add_multisig_approval

---
+++ src/app/escrow/create/set_depositor_address/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function Add_depositor_address() {
+    const router = useRouter();
+
+    return(
+        <div>
+            <h1>Default set depositor address page</h1>
+        </div>
+    );
+}
---
+++ src/app/escrow/create/set_recipient_address/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function Add_recipient_address() {
+    const router = useRouter();
+
+    return(
+        <div>
+            <h1>Default set recipient address page</h1>
+        </div>
+    );
+}
