3dec4b6e7e1c33e38aee5474add93f1d255143cb
Captain-Cork <captaincork124@DESKTOP-EVMJ62F.>
2025-04-19 17:19:10 +0200

    update import path for AppModal & ellipsify

--- src/components/ui/ui-layout.tsx
+++ src/components/ui/ui-layout.tsx
@@ -95,21 +95,3 @@ export function AppModal({
     </dialog>
   )
 }
-
-export function ellipsify(str = '', len = 4) {
-  if (str.length > 30) {
-    return str.substring(0, len) + '..' + str.substring(str.length - len, str.length)
-  }
-  return str
-}
-
-export function useTransactionToast() {
-  return (signature: string) => {
-    toast.success(
-      <div className={'text-center'}>
-        <div className="text-lg">Transaction sent</div>
-        <ExplorerLink path={`tx/${signature}`} label={'View Transaction'} className="btn btn-xs btn-primary" />
-      </div>,
-    )
-  }
-}
