fe3563aa1d55c1932a4787ecc5c78b5e3d3cf7a0
Maxime Normandin <web2@londerosports.com>
2025-05-06 21:33:40 -0400

    fix video bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,37 @@
 'use client'
 
 import Nav from '@/components/ui/nav/Nav'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Page() {
     return (
-      <div className="bg-cream h-full relative">
-        <Nav />
-        <div className="w-full h-full -mt-16">
-          <Image 
-            src="/vault.gif" 
-            alt="Vault" 
-            width={500} 
-            height={500} 
-            className="w-full h-full object-cover"
-            unoptimized={true}
-            style={{ animationIterationCount: 1 }} 
+      <div className="bg-cream min-h-screen flex flex-col">
+        <div className="relative z-20">
+          <Nav />
+        </div>
+        <div className="flex-1 relative -mt-16">
+          <video 
+            src="/vault.mp4" 
+            autoPlay 
+            muted 
+            playsInline
+            className="absolute inset-0 w-full h-full object-cover"
           />
+          <div className="absolute inset-0 bg-black opacity-70"></div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center z-10 w-full max-w-3xl px-4">
-            <h1 className="text-[55px] md:text-[75px] font-bold leading-tight text-white heading-shadow">
-              Hold Funds. Automate <span className="blue-gradient-text blue-gradient-text" data-text="Trust.">Trust.</span>
+            <h1 className="text-[35px] sm:text-[55px] md:text-[75px] font-bold leading-tight text-white heading-shadow">
+              Hold Funds.<br/> Automate <span className="blue-gradient-text blue-gradient-text" data-text="Trust.">Trust.</span>
             </h1>
-            <p className="text-white text-2xl mt-4 font-bold text-shadow">
+            <p className="text-white text-lg sm:text-2xl mt-4 font-bold text-shadow">
               Secure funds with on-chain logic<br />
               and automate releases with confidence
             </p>
             <div className="mt-8">
               <Link
                 href="/demo/escrow"
-                className="px-8 py-3 rounded-md text-white font-medium text-lg transition-colors blue-gradient-bg"
+                className="px-6 sm:px-8 py-3 rounded-md text-white font-medium text-md sm:text-xl transition-colors blue-gradient-bg"
               >
-                Try Now
+                TRY NOW
               </Link>
             </div>
           </div>
--- src/components/ui/nav/Nav.tsx
+++ src/components/ui/nav/Nav.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Nav() {
   return (
-    <nav className="relative top-4 flex items-center justify-between bg-white h-16 mx-5 sm:mx-10 md:mx-20 lg:mx-32 xl:mx-40 rounded-lg px-3 text-black">
+    <nav className="relative top-4 flex items-center justify-between bg-white h-16 mx-5 sm:mx-10 md:mx-24 lg:mx-36 xl:mx-48 rounded-lg px-3 text-black z-11">
       <div className="flex items-center gap-4">
         <Link href="/">
           <Image src="/D-logo-white.svg" alt="Logo" width={0} height={0} className="h-10 w-auto object-contain" priority />
